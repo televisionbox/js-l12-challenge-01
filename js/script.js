c3e6342ae73f76f85519d5e26c313ebdd4b5f235
Amy Rusan <36975332+televisionbox@users.noreply.github.com>
2022-09-17 15:37:06 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,4 +9,22 @@ const getImage = aync function (){
     console.log(images);
 }
 
-getImage();
+const selectRandomImage = function (images) {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  // console.log(randomIndex);
+  const randomImage = images[randomIndex];
+  // console.log(randomImage);
+  displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+  const author = randomImage.author;
+  const imageAddress = randomImage.download_url;
+  authorSpan.innerText = author;
+  img.src = imageAddress;
+  imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+  getImage();
+});
